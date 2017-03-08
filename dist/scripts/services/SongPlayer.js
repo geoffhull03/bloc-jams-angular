416449b69c5c086b18192b55a323025c0b63880e
Geoff Hull <geoff.hull@procore.com>
2017-03-07 18:51:07 -0800

    added playSong function and documentation

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -14,6 +14,7 @@
         * @desc Stops currently playing song and loads new audio file as currentBuzzObject
         * @param {Object} song
         */
+
         var setSong = function(song) {
             if (currentBuzzObject) {
                 currentBuzzObject.stop();
@@ -27,18 +28,37 @@
 
             currentSong = song;
         };
+        /**
+        *@function playSong
+        *@desc plays the current buzz song and sets the property of the object to true
+        *@param {Object} song
+        */
+        var playSong = function(song) {
+            currentBuzzObject.play();
+            song.playing = true;
+        };
 
+        /**
+        *@function SongPlayer.play
+        *@desc Takes song as the parameter. If the object song is not the same as the current song, the new song should load and start playing. Else if the buzz song is equal to the current song and it's paused, the current song will play. 
+        *@param {Object} song
+        */
         SongPlayer.play = function(song) {
             if (currentSong !== song) {
                 setSong(song);
-                currentBuzzObject.play();
-                song.playing = true;
+                playSong(song);
             } else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
                     currentBuzzObject.play();
+                    
                 }
             }
         };
+        /**
+        *@function SongPlayer.pause
+        *@desc Takes song as the parameter. If the current buzz song is paused, the song playing attribute will be set to false.    
+        *@param {Object} song
+        */
 
         SongPlayer.pause = function(song) {
             currentBuzzObject.pause();
